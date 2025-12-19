0ad6a3794f43b80326d8cc7f3847d9deb4f487ab
syamtumati <syam.rlx@gmail.com>
2025-12-19 10:34:06 +0100

    ERB: psuhed new changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -96,7 +96,7 @@ const Contact = () => {
 
             <div className="map-container">
               <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!..."
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2653.87589956818!2d8.5753183!3d50.2070936!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47bd101a25316c71%3A0xda28d3d96d7054eb!2sHohemarkstra%C3%9Fe%2075%2C%2061440%20Oberursel%20(Taunus)!5e0!3m2!1sen!2sde!4v1700000000000"
                 width="100%"
                 height="280"
                 style={{ border: 0 }}
