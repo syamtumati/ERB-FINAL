ec54d96099b941e08e9ae7aaf667f542b05d55fa
syamtumati <syam.rlx@gmail.com>
2025-12-20 06:43:56 +0100

    Allow public Vite YouTube API key

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 5173,
-  },
-  base: "/ERB-FINAL/",
+export default defineConfig(() => {
+  const isNetlify = !!process.env.NETLIFY;
+  const isGithubPages = !!process.env.GITHUB_PAGES;
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 5173,
+    },
+    base: isNetlify
+      ? "/"
+      : isGithubPages
+      ? "/ERB-FINAL/"
+      : "/",
+  };
 });
 
