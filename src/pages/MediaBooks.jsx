81fd762521f2eeb0372da9bbb19e9e787cf3c495
syamtumati <syam.rlx@gmail.com>
2026-01-23 20:08:17 +0100

    added books

--- src/pages/MediaBooks.jsx
+++ src/pages/MediaBooks.jsx
@@ -6,6 +6,11 @@ import missionMp3 from "../assets/books/gemeinde-und-mission-hoerbuch.jpg";
 import missionEbook from "../assets/books/gemeinde-und-mission-ebook.jpg";
 import brautwerbungEbook from "../assets/books/biblische-brautwerbung-ebook.jpg";
 import treueMaennerEbook from "../assets/books/treue-maenner-wer-findet-sie-ebook.jpg";
+import eineFrauEbook from "../assets/books/eine-frau-die-zu-ruehmen-ist-ebook.jpg";
+import treueMaennerPrint from "../assets/books/treue-maenner-wer-findet-sie-print.jpg";
+import brautwerbungPrint from "../assets/books/biblische-brautwerbung-print.jpg";
+import eineFrauPrint from "../assets/books/eine-frau-die-zu-ruehmen-ist-print.jpg";
+import missionPrint from "../assets/books/gemeinde-und-mission-print.jpg";
 
 export default function MediaBooks() {
   const { i18n } = useTranslation();
@@ -31,6 +36,31 @@ export default function MediaBooks() {
       title: "Treue Männer – wer findet sie? – eBook",
       image: treueMaennerEbook,
       link: "https://www.voh-shop.de/Treue-Maenner-wer-findet-sie-eBook-Download.html"
+    },
+    {
+    title: "Eine Frau, die zu rühmen ist – eBook",
+    image: eineFrauEbook,
+    link: "https://www.voh-shop.de/Eine-Frau-die-zu-ruehmen-ist-eBook-Download.html"
+    },
+    {
+    title: "Treue Männer – wer findet sie?",
+    image: treueMaennerPrint,
+    link: "https://www.voh-shop.de/Treue-Maenner-wer-findet-sie.html"
+    },
+    {
+    title: "Biblische Brautwerbung",
+    image: brautwerbungPrint,
+    link: "https://www.voh-shop.de/Biblische-Brautwerbung.html"
+    },
+    {
+    title: "Eine Frau, die zu rühmen ist",
+    image: eineFrauPrint,
+    link: "https://www.voh-shop.de/Eine-Frau-die-zu-ruehmen-ist.html"
+    },
+    {
+    title: "Gemeinde und Mission",
+    image: missionPrint,
+    link: "https://www.voh-shop.de/Gemeinde-und-Mission.html"
     }
   ];
 
